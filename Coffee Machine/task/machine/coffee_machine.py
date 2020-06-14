8dcf96c1ae5d3a18cba02b6fc05d06ec3525e891
Tsairenkun <Tsairenkun@gmail.com>
2020-06-14 21:15:36 +0800

    Coffee Machine part 3

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -1,4 +1,7 @@
 # Write your code here
+
+import math
+
 print("""Starting to make a coffee
 Grinding coffee beans
 Boiling water
@@ -7,13 +10,36 @@ Pouring coffee into the cup
 Pouring some milk into the cup
 Coffee is ready!""")
 
-cups = int(input("Write how many cups of coffee you will need: >"))
+
+# Part 2
+# cups = int(input("Write how many cups of coffee you will need: >"))
 
 water = 200
 milk = 50
 beans = 15
 
-print("For " + str(cups)  + " cups of coffee you will need:")
-print(str(cups * water) + "ml of water")
-print(str(cups * milk) + "ml of milk")
-print(str(cups * beans) + "g of beans")
+# print("For " + str(cups)  + " cups of coffee you will need:")
+# print(str(cups * water) + "ml of water")
+# print(str(cups * milk) + "ml of milk")
+# print(str(cups * beans) + "g of beans")
+
+# part 3
+water_left = int(input("Write how many ml of water the coffee machine has:> "))
+milk_left = int(input("Write how many ml of milk the coffee machine has:> "))
+beans_left = int(input("Write how many grams of coffee beans the coffee machine has:> "))
+cups_needed = int(input("Write how many cups of coffee you will need:> "))
+
+able_water = math.floor(water_left / water)
+able_milk = math.floor(milk_left / milk)
+able_beans = math.floor(beans_left / beans)
+
+list = [able_beans, able_water, able_milk]
+
+if min(list) == cups_needed:
+	print("Yes, I can make that amount of coffee")
+elif min(list) > cups_needed:
+	print ("Yes, I can make that amount of coffee", "(and even " + str(min(list) - cups_needed) + " more than that)" )
+elif min(list) < cups_needed:
+	print ("No, I can make only " + str(min(list)) + " cups of coffee")
+
+
