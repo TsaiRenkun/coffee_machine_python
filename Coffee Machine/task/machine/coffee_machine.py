8dabed3325e84415828616fbee09f592335b309c
Tsairenkun <Tsairenkun@gmail.com>
2020-06-23 11:25:48 +0800

    Coffee Machine part 4 adding some more input

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -1,5 +1,6 @@
 # Write your code here
 
+
 import math
 
 print("""Starting to make a coffee
@@ -27,8 +28,8 @@ beans = 15
 water_left = int(input("Write how many ml of water the coffee machine has:> "))
 milk_left = int(input("Write how many ml of milk the coffee machine has:> "))
 beans_left = int(input("Write how many grams of coffee beans the coffee machine has:> "))
-# milk_left = int(input("Write how many ml of milk the coffee machine has:> "))
-# beans_left = int(input("Write how many grams of coffee beans the coffee machine has:> "))
+# # milk_left = int(input("Write how many ml of milk the coffee machine has:> "))
+# # beans_left = int(input("Write how many grams of coffee beans the coffee machine has:> "))
 # cups_needed = int(input("Write how many cups of coffee you will need:> "))
 #
 # able_water = math.floor(water_left / water)
@@ -58,9 +59,80 @@ print(str(beans_left) + " of coffee beans")
 print(str(dis_cups) + " of disposable cups")
 print(str(money) + " of money")
 
-print(input("Write action(buy, fill, take): >"))
+choice_input = str(input("Write action(buy, fill, take): > "))
 
-print(input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: >"))
+if(choice_input == "buy"):
+	print("buying")
+
+	def espresso():
+		water_needed = 250
+		milk_needed = 0
+		coffee_needed = 16
+		cost = 4
+		return water_needed,milk_needed,coffee_needed,cost
+
+	def latte():
+		water_needed = 350
+		milk_needed = 75
+		coffee_needed = 20
+		cost = 7
+		return water_needed,milk_needed,coffee_needed,cost
 
 
+	def cappuccino():
+		water_needed = 200
+		milk_needed = 100
+		coffee_needed = 12
+		cost = 6
+		return water_needed, milk_needed, coffee_needed,cost
+
+	def buying_drink(i):
+		switcher = {
+			1: espresso(),
+			2: latte(),
+			3: cappuccino()
+		}
+		res = switcher.get(i, "Invalid choice of drink")
+		return res
+
+	res = buying_drink(int(input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: > ")))
+
+	print("The coffee machine has:")
+	print(res)
+	print(str(water_left - res[0]) + " of water")
+	print(str(milk_left - res[1]) + " of milk")
+	print(str(beans_left - res[2]) + " of coffee beans")
+	print(str(dis_cups - 1) + " of disposable cups")
+	print(str(money + res[3]) + " of money")
+
+elif(choice_input == "fill"):
+
+
+	add_water =	int(input("Write how many ml of water do you want to add: > "));
+	add_milk =	int(input("Write how many ml of milk do you want to add: > "));
+	add_coffee = int(input("Write how many grams of coffee beans do you want to add: > "));
+	add_cup =	int(input("Write how many disposable cups of coffee do you want to add: > "));
+
+	print("The coffee machine has:")
+	print(str(water_left + add_water) + " of water")
+	print(str(milk_left + add_milk) + " of milk")
+	print(str(beans_left + add_coffee) + " of coffee beans")
+	print(str(dis_cups + add_cup) + " of disposable cups")
+	print(str(money) + " of money")
+
+elif(choice_input == "take"):
+	print("taking")
+
+	print("I gave you $" + str(money));
+
+	money = 0
+
+	print("The coffee machine has:")
+	print(str(water_left) + " of water")
+	print(str(milk_left) + " of milk")
+	print(str(beans_left) + " of coffee beans")
+	print(str(dis_cups) + " of disposable cups")
+	print(str(money) + " of money")
+else:
+	print("othering")
 
