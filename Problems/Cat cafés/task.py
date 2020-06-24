a6a676a78250bfdfcbfb95d935bb5650faa9547a
Tsairenkun <Tsairenkun@gmail.com>
2020-06-24 15:12:58 +0800

    Doing cat cafes

--- Problems/Cat cafés/task.py
+++ Problems/Cat cafés/task.py
@@ -1,9 +1,14 @@
-data = []
 
-while True:
-	shop = str(input().split())
-	print(shop)
-	data.append(shop)
-	if shop == "MEOW":
-		break
+name = input()
+cafe = []
+while name != "MEOW":
+    cafe += name.split()
+    name = input()
 
+largest_index = 1
+for i in range(1, len(cafe), 2):
+    cafe[i] = int(cafe[i])
+    if cafe[i] > cafe[largest_index]:
+        largest_index = i
+
+print(cafe[largest_index - 1])
